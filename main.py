4ec3b163fdd54f64a9425207fd78b79f5df17abf
dorick <dorick2301@mail.ru>
2026-06-03 22:32:25 +0300

    add

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Aeroplane Tracker - Система отслеживания самолётов
+"""
+
+from src.user_interface import user_interaction
+
+
+def main():
+    """Главная функция программы"""
+    user_interaction()
+
+
+if __name__ == "__main__":
+    main()
